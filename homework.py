9c431bada87838fd5140d6a228e2b9f8d5f8ab25
vovanbart <vovanbart@icloud.com>
2021-08-08 15:15:30 +0300

    2 спринт.Проект.

--- homework.py
+++ homework.py
@@ -2,8 +2,72 @@ import datetime as dt
 
 
 class Calculator:
-    pass
 
+    def __init__(self, limit):
+        self.limit = limit
+        self.records = []
+        self.today = dt.date.today()
+        self.week_ago = self.today - dt.timedelta(days=7)
+
+    def add_record(self, record):
+        self.records.append(record)
+
+    def get_today_stats(self):
+        today_stats = []
+        for record in self.records:
+            if record.date == self.today:
+                today_stats.append(record.amount)
+        return sum(today_stats)
+
+    def get_week_stats(self):
+        week_stats = []
+        for record in self.records:
+            if self.week_ago <= record.date <= self.today:
+                week_stats.append(record.amount)
+        return sum(week_stats)
+
+    def get_today_remains(self):
+        remains = self.limit - self.get_today_stats()
+        return remains
 
 class Record:
-    pass
+    def __init__(self, amount, comment, date=None):
+        self.amount = amount
+        self.comment = comment
+        if date is None:
+            self.date = dt.date.today()
+        else:
+            self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
+
+class CashCalculator(Calculator):
+    USD_RATE = 70.0
+    EURO_RATE = 77.0
+    RUB_RATE = 1
+    def get_today_cash_remained(self, currency='rub'):
+        currencies = {'usd': ('USD', CashCalculator.USD_RATE),
+                      'eur': ('Euro', CashCalculator.EURO_RATE),
+                      'rub': ('руб', CashCalculator.RUB_RATE)}
+        cash_remained = self.get_today_remains()
+        if cash_remained == 0:
+            return 'Денег нет, держись'
+        if currency not in currencies:
+            return f'Валюта {currency} не поддерживается'
+        name, rate = currencies[currency]
+        cash_remained = round(cash_remained / rate, 2)
+        if cash_remained > 0:
+            message = f'На сегодня осталось {cash_remained} {name}'
+        else:
+            cash_remained = abs(cash_remained)
+            message = (f'Денег нет, держись: твой долг - {cash_remained} '
+                       f'{name}')
+        return message
+
+class CaloriesCalculator(Calculator):
+    def get_calories_remained(self):
+        calories_remained = self.get_today_remains()
+        if calories_remained > 0:
+            message = (f'Сегодня можно съесть что-нибудь ещё, но с общей '
+                       f'калорийностью не более {calories_remained} кКал')
+        else:
+            message = 'Хватит есть!'
+        return message
